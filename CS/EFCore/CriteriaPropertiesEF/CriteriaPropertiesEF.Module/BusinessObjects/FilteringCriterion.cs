7c9a8eba0720942f94082c22719842aa1f29a22f
Elena Khamlyuk <elena.khamlyuk@devexpress.com>
2024-06-26 12:11:16 +0400

    code

--- CS/EFCore/CriteriaPropertiesEF/CriteriaPropertiesEF.Module/BusinessObjects/FilteringCriterion.cs
+++ CS/EFCore/CriteriaPropertiesEF/CriteriaPropertiesEF.Module/BusinessObjects/FilteringCriterion.cs
@@ -1,21 +1,40 @@
-using System;
 using DevExpress.ExpressApp;
-using DevExpress.Persistent.Base;
-using DevExpress.Persistent.BaseImpl;
-using DevExpress.Persistent.Validation;
+using DevExpress.ExpressApp.DC;
 using DevExpress.ExpressApp.Editors;
-using DevExpress.ExpressApp.Utils;
-using System.ComponentModel;
+using DevExpress.Persistent.Base;
 using DevExpress.Persistent.BaseImpl.EF;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HowToUseCriteriaPropertyEditors.Module {
     [DefaultClassOptions, ImageName("Action_Filter")]
     public class FilteringCriterion : BaseObject {
+        private Type objectType;
+
         public virtual string Description { get; set; }
-        [ImmediatePostData]
-        public virtual Type ObjectType { get; set; }
-        [CriteriaOptions("ObjectType")]
+
+        [Browsable(false)]
+        public virtual string ObjectTypeName {
+            get { return objectType == null ? string.Empty : objectType.FullName; }
+            set {
+                ITypeInfo typeInfo = XafTypesInfo.Instance.FindTypeInfo(value);
+                objectType = typeInfo == null ? null : typeInfo.Type;
+            }
+        }
+
+        [NotMapped, ImmediatePostData]
+        public virtual Type ObjectType {
+            get { return objectType; }
+            set {
+                if (objectType == value)
+                    return;
+                objectType = value;
+                Criterion = string.Empty;
+            }
+        }
+
+        [CriteriaOptions(nameof(ObjectType))]
+        [FieldSize(FieldSizeAttribute.Unlimited)]
         [EditorAlias(EditorAliases.PopupCriteriaPropertyEditor)]
         public virtual string Criterion { get; set; }
     }
